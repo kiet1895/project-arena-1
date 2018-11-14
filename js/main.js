5167bb6ff01b7606c329c496d12d001df6943463
kiet1895 <kiet1895@gmail.com>
2018-11-14 22:17:21 +0700

    web arena

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(function ($) {
+    $(document).ready(function () {
+        $('.characters').owlCarousel({
+            items: 5,
+            loop: true,
+            center: true,
+            nav: true,
+            merge: true,
+            smartSpeed: 800,
+            responsive: {
+
+            }
+        });
+    });
+})(jQuery);
